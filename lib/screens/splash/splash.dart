491850da921769fa8a3729564c01576f26d6b9b1
Athulraj K <athulrajchuzali@gmail.com>
2024-04-10 09:53:12 +0530

    splash changes

--- lib/screens/splash/splash.dart
+++ lib/screens/splash/splash.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:scratch_application/screens/onboarding/onboarding%20screen.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -12,27 +14,35 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
 
-  @override
-  void initState() {
-    splashscreenmethod();
-    super.initState();
-  }
+  // @override
+  // void initState() {
+  //   splashscreenmethod();
+  //   super.initState();
+  // }
 
-  void splashscreenmethod(){
-    Timer(const Duration(seconds: 3), () {
+  // void splashscreenmethod(){
+  //   Timer(const Duration(seconds: 3), () {
 
-      Navigator.of(context).push(
-        MaterialPageRoute(builder: (context) => const OnboardingScreen())
-      );
-     });
-  }
+  //     Navigator.of(context).push(
+  //       MaterialPageRoute(builder: (context) => const OnboardingScreen())
+  //     );
+  //    });
+  // }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Image.asset("assets/logo/tasklogo.png"),
-      ),
-    );
+    return AnimatedSplashScreen(
+        splash: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            LottieBuilder.asset("assets/Lottie/animated.json")
+          ],
+        ), 
+        nextScreen: const OnboardingScreen(),
+        splashIconSize: 420,
+        backgroundColor: Colors.white,
+        duration: 4000,
+        );
+    
+
   }
 }
